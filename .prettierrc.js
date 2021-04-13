f0d1dc2c3f455095cadfff672f36710657ebfa6d
lvyueying <lvyueying@mininglamp.com>
2021-04-13 14:31:43 +0800

    配置eslint prittier

---
+++ .prettierrc.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-13 14:30:03
+ * @LastEditTime: 2021-04-13 14:30:33
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /new-react-learn/.prettierrc.js
+ */
+//prittierrc.js
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+};
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2021-04-13 14:10:55
- * @LastEditTime: 2021-04-13 14:15:23
- * @LastEditors: your name
+ * @LastEditTime: 2021-04-13 14:30:59
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /new-react-learn/src/App.js
  */
-import "./App.less";
-import { Button } from "antd";
+import React from 'react';
+
+import './App.less';
+import { Button } from 'antd';
 
 function App() {
   return (
